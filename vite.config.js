dc63929b17c9397905402ed7f8851c70b52b2e0c
SPN <10730985+splesanaden@users.noreply.github.com>
2023-08-03 10:52:20 -0600

    1

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,9 @@ import vue from "@vitejs/plugin-vue";
 
 import svgLoader from "vite-svg-loader";
 
-import vercel from "vite-plugin-vercel";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader(), vercel()],
+  plugins: [vue(), svgLoader()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
